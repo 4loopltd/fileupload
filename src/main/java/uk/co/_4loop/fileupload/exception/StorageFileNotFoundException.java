53e35201058ac6f8a7d18d03a0fcd05be9a68b22
4loopltd <phil@4loop.co.uk>
2019-04-14 22:20:15 +0100

    Initial commit
    dfasdf

---
+++ src/main/java/uk/co/_4loop/fileupload/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package uk.co._4loop.fileupload.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
